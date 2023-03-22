69c2813071fd4258489bfb9e4f8b6983b2698596
Jules Pietri <heahdude@yahoo.fr>
2023-03-22 16:19:29 +0100

    refactor: use set up

--- tests/Bowling/BowlingGameTest.php
+++ tests/Bowling/BowlingGameTest.php
@@ -7,25 +7,28 @@ use PHPUnit\Framework\TestCase;
 
 class BowlingGameTest extends TestCase
 {
+    private BowlingGame $bowlingGame;
+
+    protected function setUp(): void
+    {
+        $this->bowlingGame = new BowlingGame();
+    }
+
     public function testAllGutter()
     {
-        $bowlingGame = new BowlingGame();
-
         for ($i = 0; $i < 20; ++$i) {
-            $bowlingGame->roll(0);
+            $this->bowlingGame->roll(0);
         }
 
-        self::assertSame(0, $bowlingGame->score());
+        self::assertSame(0, $this->bowlingGame->score());
     }
 
     public function testAllOnes()
     {
-        $bowlingGame = new BowlingGame();
-
         for ($i = 0; $i < 20; ++$i) {
-            $bowlingGame->roll(1);
+            $this->bowlingGame->roll(1);
         }
 
-        self::assertSame(20, $bowlingGame->score());
+        self::assertSame(20, $this->bowlingGame->score());
     }
 }
