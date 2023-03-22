6b08acebc4e96e3ac17b83d062fa12f63a0bc7dd
Jules Pietri <heahdude@yahoo.fr>
2023-03-22 11:38:15 +0100

    red: fibonacci 3

--- tests/FibonacciTest.php
+++ tests/FibonacciTest.php
@@ -12,6 +12,7 @@ class FibonacciTest extends TestCase
             0 => 0,
             1 => 1,
             2 => 1,
+            3 => 2,
         ];
 
         foreach ($cases as $index => $case) {
