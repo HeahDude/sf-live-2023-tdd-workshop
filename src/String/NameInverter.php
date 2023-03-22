a68b7ab4eeb5be823aa0c286d27edac2c9ac56c5
Jules Pietri <heahdude@yahoo.fr>
2023-03-22 14:43:03 +0100

    refactor: remove comments in favor of private methods to improve readibility

--- src/String/NameInverter.php
+++ src/String/NameInverter.php
@@ -12,15 +12,31 @@ class NameInverter
 
         $nameParts = explode(' ', $name);
 
-        if (1 === count($nameParts)) { // handle simple name
+        if ($this->isSimpleName($nameParts)) {
             return $name;
         }
 
-        // handle honorifics
-        if ('Mr.' === $nameParts[0]) { // starts with honorifics
-            array_shift($nameParts); // remove honorifics
+        if ($this->hasHonorifics($nameParts)) {
+            $nameParts = $this->removeHonorifics($nameParts);
         }
 
         return sprintf('%s, %s', $nameParts[1], $nameParts[0]);
+    }
+
+    private function isSimpleName(array $nameParts): bool
+    {
+        return 1 === count($nameParts);
+    }
+
+    private function hasHonorifics(array $nameParts): bool
+    {
+        return 'Mr.' === $nameParts[0];
+    }
+
+    private function removeHonorifics(array $nameParts): array
+    {
+        array_shift($nameParts);
+
+        return $nameParts;
     }
 }
