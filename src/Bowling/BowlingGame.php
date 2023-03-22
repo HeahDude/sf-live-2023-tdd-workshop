ec9e79aa66e756fc8b8f09a62a7b9ba44747d14b
Jules Pietri <heahdude@yahoo.fr>
2023-03-22 16:13:32 +0100

    green: fake all gutter

---
+++ src/Bowling/BowlingGame.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Bowling;
+
+class BowlingGame
+{
+    public function roll(int $pins): void
+    {
+    }
+
+    public function score(): int
+    {
+        return 0;
+    }
+}
